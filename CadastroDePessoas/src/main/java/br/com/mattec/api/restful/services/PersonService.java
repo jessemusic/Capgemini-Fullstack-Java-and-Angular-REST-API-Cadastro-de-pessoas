64ff9e4a6c8a89d78ad1a50fa1700b48a7dd6072
jessemusic <jmc_cursos@hotmail.com>
2021-10-14 18:42:38 -0300

    Refatoração de código

--- CadastroDePessoas/src/main/java/br/com/mattec/api/restful/services/PersonService.java
+++ CadastroDePessoas/src/main/java/br/com/mattec/api/restful/services/PersonService.java
@@ -26,16 +26,13 @@ public class PersonService {
     public MessageResponseDTO create(PersonDTO personDTO) {
         Person person = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(person);
-
         MessageResponseDTO messageResponse = createMessageResponse("Person successfully created with ID ", savedPerson.getId());
-
         return messageResponse;
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
         Person person = personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
-
         return personMapper.toDTO(person);
     }
 
@@ -52,7 +49,6 @@ public class PersonService {
 
         Person updatedPerson = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(updatedPerson);
-
         MessageResponseDTO messageResponse = createMessageResponse("Person successfully updated with ID ", savedPerson.getId());
 
         return messageResponse;
@@ -61,7 +57,6 @@ public class PersonService {
     public void delete(Long id) throws PersonNotFoundException {
         personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
-
         personRepository.deleteById(id);
     }
 
